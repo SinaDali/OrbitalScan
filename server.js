a55fa189cf8ff4b8d959e4a2ab1d1fa8b371c5fa
SinaDali <mokarisalmasisina@gmail.com>
2025-04-10 22:21:29 +0330

    Fixed routing for pages

--- server.js
+++ server.js
@@ -6,23 +6,26 @@ const port = process.env.PORT || 10000;  // پورت سرور
 
 // Serve the home page (index.html)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));  // صفحه اصلی که به نظر شما می‌خواهید
+    res.sendFile(path.join(__dirname, 'index.html'));  // صفحه اصلی
 });
 
 // Serve alpha.html
 app.get('/alpha', (req, res) => {
-    res.sendFile(path.join(__dirname, 'alpha.html'));  // مطمئن شوید که فایل در ریشه پروژه قرار دارد
+    res.sendFile(path.join(__dirname, 'alpha.html'));  // صفحه alpha
 });
 
 // Serve nft.html
 app.get('/nft', (req, res) => {
-    res.sendFile(path.join(__dirname, 'nft.html'));
+    res.sendFile(path.join(__dirname, 'nft.html'));  // صفحه nft
 });
 
 // Serve about.html
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'));
+    res.sendFile(path.join(__dirname, 'about.html'));  // صفحه about
 });
+
+// Serve static files (like images, CSS)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen on the port
 app.listen(port, () => {
