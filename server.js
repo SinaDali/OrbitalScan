fd1fa026dc5680efc65707d64dcf22a1480663c3
SinaDali <mokarisalmasisina@gmail.com>
2025-04-10 22:13:40 +0330

    Fixed server routes

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 10000;  // استفاده از پورت مشخص شده
+const port = process.env.PORT || 10000;  // پورت سرور
+
+// Serve the home page (index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));  // صفحه اصلی که به نظر شما می‌خواهید
+});
 
 // Serve alpha.html
 app.get('/alpha', (req, res) => {
-    res.sendFile(path.join(__dirname, 'alpha.html'));
+    res.sendFile(path.join(__dirname, 'alpha.html'));  // مطمئن شوید که فایل در ریشه پروژه قرار دارد
 });
 
-// Serve other pages
+// Serve nft.html
 app.get('/nft', (req, res) => {
     res.sendFile(path.join(__dirname, 'nft.html'));
 });
