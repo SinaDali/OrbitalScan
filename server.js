305c51dfb123a46fdef17b6408c7f92dfd75d359
SinaDali <mokarisalmasisina@gmail.com>
2025-04-12 03:08:03 +0330

    fix: correct server.js routes

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// برای دسترسی به فایل‌های استاتیک (public)
+// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// روت برای صفحه اصلی
+// Serve HTML files
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// روت برای سایر صفحات
 app.get('/alpha', (req, res) => {
   res.sendFile(path.join(__dirname, 'alpha.html'));
 });
@@ -28,27 +27,11 @@ app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'about.html'));
 });
 
-// اگر صفحه‌ای پیدا نشد
+// 404 Page Not Found
 app.use((req, res) => {
   res.status(404).send('Page Not Found');
 });
 
-// سرور را ران کن
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-});
-app.get('/alpha', (req, res) => {
-  res.sendFile(path.join(__dirname, 'alpha.html'));
-});
-
-app.get('/nft', (req, res) => {
-  res.sendFile(path.join(__dirname, 'nft.html'));
-});
-
-app.get('/airdrops', (req, res) => {
-  res.sendFile(path.join(__dirname, 'airdrops.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.html'));
 });
